2d0bc960c7edbe678e46dd96b3e60aa9b2675218
giorgizena123 <zenaishvili621@gmail.com>
2024-11-25 14:50:35 +0400

    Create time.java

---
+++ HOMEWORKS/ciklebis davaleba 1/class/src/time.java
@@ -0,0 +1,14 @@
+public class time {
+    int hour;
+    int min;
+    int sec;
+    time(int h,int m,int s){
+        hour=h;
+        min=m;
+        sec=s;
+    }
+    void time(){
+        System.out.println(hour+":"+min+":"+sec);
+    }
+
+}
